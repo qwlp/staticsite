54c62268e47492374a96343cffae731840eccf0b
Sokpiseth Thin <sokpiseth.thin@gmail.com>
2025-10-19 01:32:28 +0700

    full

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from copy_helper import copy_to_public
 from template_gen import generate_page, generate_pages_recursive
 
@@ -7,6 +8,7 @@ dir_path_static = "./static"
 dir_path_public = "./public"
 dir_path_content = "./content"
 template_path = "./template.html"
+basepath = sys.argv[1]
 
 
 def main():
@@ -14,7 +16,7 @@ def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     copy_to_public()
-    generate_pages_recursive("./content/", "./template.html", "./public/")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 
 main()
